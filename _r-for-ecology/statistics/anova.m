f5f1b778a3d73d1aa7f86ec25c57348966dc8f44
jlcom <jlcombitac@unal.edu.co>
2025-10-14 20:54:36 -0500

    Create anova.m

---
+++ _r-for-ecology/statistics/anova.m
@@ -0,0 +1,6 @@
+---
+title: "ANOVA and model comparison"
+category: "Statistics"
+---
+
+Perform analysis of variance (ANOVA) and model comparison using **aov()**, **anova()**, and the **car** package.
