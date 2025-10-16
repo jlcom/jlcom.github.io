c0ad74e2ac37d3763c4aa27e91dabeeca66bd376
jlcom <jlcombitac@unal.edu.co>
2025-10-15 20:51:49 -0500

    Create index.m

---
+++ _r/01_r-base/index.m
@@ -0,0 +1,20 @@
+---
+layout: page
+title: "R Base"
+permalink: /r/01_r-base/
+---
+
+# R Base 🧠
+
+Welcome to the **R Base** section.  
+Here you'll learn the foundations of R programming — essential for any ecological analysis.
+
+---
+
+## 📘 Modules
+
+{% for post in site.r %}
+  {% if post.path contains '01_r-base/' and post.name != 'index.md' %}
+  - [{{ post.title }}]({{ post.url }})
+  {% endif %}
+{% endfor %}
